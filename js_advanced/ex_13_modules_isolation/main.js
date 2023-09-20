b36a97442de47f033373cc2a861c2ae07c5792c9
vijay <vijay.nvb@gmail.com>
2023-09-20 15:56:14 +0530

    example 10 to 13

---
+++ js_advanced/ex_13_modules_isolation/main.js
@@ -0,0 +1,20 @@
+
+// where you get the code into main file and then execute 
+require('./employee.js')
+require('./parttimeemp.js') 
+
+
+
+//module iife imediately invoked function execute
+// (function(__dirname, __filename,Modules, exports, require){
+// const name = 'part time employee';
+// console.log(name);
+// })();
+
+
+// (function(){
+// const name = 'full time employee';
+// console.log(name);
+// })()
+
+console.log("main file ")
